ece1bea53c74c46ec8d10fc6121d04d530da95e6
RavingSmurfGB <josephhutchinson4@gmail.com>
2021-09-24 11:16:34 +0100

    sadasd

---
+++ client.py
@@ -0,0 +1,6 @@
+import datetime, random
+import winsound
+# Play Windows exit sound.
+winsound.PlaySound("Bellatrix.wav", winsound.SND_FILENAME)
+
+#winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
